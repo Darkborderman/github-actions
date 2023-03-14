89ca7c2b25eb9fe768526c61f551bae0550879dd
Darkborderman <reastw1234@gmail.com>
2023-03-14 15:31:05 +0800

    refactor: Port script from old repo

---
+++ github-repo-info/script.sh
@@ -0,0 +1,5 @@
+sudo apt-get install jq -y
+gh repo edit --add-topic `jq -r '.topics | join(",")' gh_config.json`
+gh repo edit --homepage `jq -r '.homepage' gh_config.json`
+gh repo edit --description "`jq -r '.description' gh_config.json`"
+gh repo edit --enable-wiki=`jq -r '.enable_wiki' gh_config.json`
